8d04f387b589038d23dc3d0473543eca9fa14979
Nimesh Maharjan <maharjannimesh11@gmail.com>
2022-06-08 15:57:49 +0545

    hehe

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/image-viewer/",
+  base: "/image-zoom-vue/",
   plugins: [vue()],
   resolve: {
     alias: {
